05e2b16b6d6af0087e117561be651f2fa10d78df
LilTOJustice <muianick4@gmail.com>
2023-08-13 23:11:37 -0700

    Finished implementing ring functionality

--- src/main/java/com/fishingbell/BellFishingBobberEntity.java
+++ src/main/java/com/fishingbell/BellFishingBobberEntity.java
@@ -4,10 +4,11 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class BellFishingBobberEntity extends FishingBobberEntity {
     public BellFishingBobberEntity(EntityType<? extends FishingBobberEntity> entityType, World world) {
@@ -22,29 +23,29 @@ public class BellFishingBobberEntity extends FishingBobberEntity {
 
     private void ring()
     {
-        getWorld().playSound(null, getPlayerOwner().getBlockPos(), SoundEvents.BLOCK_BELL_USE, SoundCategory.BLOCKS, 2.0f, 1.0f);
+        getWorld().playSound(null, Objects.requireNonNull(getPlayerOwner()).getBlockPos(), SoundEvents.BLOCK_BELL_USE, SoundCategory.BLOCKS, 2.0f, 1.0f);
     }
 
     @Override
     public void tick() {
         super.tick();
-        if (last == false && caughtFish == true)
+        if (!last && caughtFish)
         {
             last = caughtFish;
             ring();
         }
-        else if (caughtFish == false)
+        else if (!caughtFish)
         {
             last = caughtFish;
         }
     }
 
     @Override
-    private boolean removeIfInvalid(PlayerEntity player) {
+    protected boolean removeIfInvalid(PlayerEntity player) {
         ItemStack itemStack = player.getMainHandStack();
         ItemStack itemStack2 = player.getOffHandStack();
-        boolean bl = itemStack.isOf(Items.BELL_FISHING_ROD);
-        boolean bl2 = itemStack2.isOf(Items.BELL_FISHING_ROD);
+        boolean bl = itemStack.isOf(FishingBell.BELL_FISHING_ROD);
+        boolean bl2 = itemStack2.isOf(FishingBell.BELL_FISHING_ROD);
         if (player.isRemoved() || !player.isAlive() || !bl && !bl2 || this.squaredDistanceTo(player) > 1024.0) {
             this.discard();
             return true;
